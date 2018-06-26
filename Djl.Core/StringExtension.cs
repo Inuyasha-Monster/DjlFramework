976e6e7b5e1c9052591c23994e9a199a7af785a9
djlnet <djlnet@17track.cn>
2018-06-26 17:49:05 +0800

    增加且初始化 djl.core 基础标准库

---
+++ Djl.Core/StringExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Djl.Core
+{
+    public static class StringExtension
+    {
+        public static bool IsNullOrEmpty(this string str)
+        {
+            return string.IsNullOrEmpty(str);
+        }
+
+        public static bool IsNullOrWhiteSpace(this string str)
+        {
+            return string.IsNullOrWhiteSpace(str);
+        }
+    }
+}
