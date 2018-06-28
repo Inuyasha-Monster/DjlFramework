5ae0342b792dce42f249a7a6fcfd5335a5ee2f62
djlnet <djlnet@17track.cn>
2018-06-28 10:22:17 +0800

    增加一些扩展类

---
+++ Djl.Core/Extension/ExceptionExtension.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Djl.Core.Extension
+{
+    public static class ExceptionExtension
+    {
+        #region 获取最底层异常
+        /// <summary>
+        /// 获取最底层异常
+        /// </summary>
+        public static Exception GetDeepestException(this Exception ex)
+        {
+            var innerException = ex.InnerException;
+            var resultExcpetion = ex;
+            while (innerException != null)
+            {
+                resultExcpetion = innerException;
+                innerException = innerException.InnerException;
+            }
+            return resultExcpetion;
+        }
+        #endregion
+    }
+}
